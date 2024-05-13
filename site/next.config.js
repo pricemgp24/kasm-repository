7713f6657ce15f83e14fb12532764be80cd4d1c2
pricemgp24 <74324270+pricemgp24@users.noreply.github.com>
2024-05-13 17:32:50 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,8 +2,8 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Glenn Networking',
+    description: 'Hopefully Getting Networking CLasses working for everyone.',
     icon: '/img/logo.svg',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
